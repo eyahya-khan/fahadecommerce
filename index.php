2d1fe33a3b7849bab5020b7599e912828f4c15be
eyahya khan <eyahyakhan@eyahyas-MacBook-Pro.local>
2020-08-03 21:56:54 +0200

    design index/product page

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 require('src/dbconnect.php');
-
 //fetch all information
     try {
   $query = "SELECT * FROM products;";
@@ -10,16 +9,12 @@ require('src/dbconnect.php');
   throw new \PDOException($e->getMessage(), (int) $e->getCode());
 }
 ?>
-
 <?php include('layout/header.php');?>
-
 <!DOCTYPE html>
 <html lang="en">
-
 <body>
     <div class="container">
-        <h2>Sweden Bangla trade venture background image</h2>
-
+<!--        <h2>Sweden Bangla trade venture background image</h2>-->
         <div class="row">
             <div class="col-12">
                 <a href="login.php" class="float-right">| Log in</a>
@@ -38,36 +33,24 @@ require('src/dbconnect.php');
                 </video>
             </div>
         </div>
-
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                </tr>
-            </thead>
-            <tbody class="text-dark text-center">
-                <?php foreach ($products as $key => $product) { ?>
-                <tr>
-                    <td><a href="product.php?hidID=<?=$product['id']?>"><img src="image/<?php echo $product['img']; ?>" width="50px" height="50px"></a></td>
-                    <td><a href="product.php?hidID=<?=$product['id']?>" class="text-dark"><?php echo $product['name']; ?> </a></td>
-                    <td><a href="product.php?hidID=<?=$product['id']?>" class="text-dark"><?php echo $product['price']; ?> taka/item</a></td>
-                    <td><?php echo $product['quantity']; ?> items available</td>
-                    <td>
-
-                        <!--counting first line-->
-                        <?php
-                    $pos = strpos($product['description'], '.');
-                    $firstSentence = substr($product['description'], 0, max($pos+1, 40));
-                    echo $firstSentence;
-                    ?>
-                        <!--sending id to product.php page for fetching specific data-->
-                        <br><a href="product.php?hidID=<?=$product['id']?>"><em>more info</em></a>
-                    </td>
-
-                </tr>
-                <?php } ?>
-            </tbody>
-        </table>
-
-
+        
+        <div class="row shadow-lg mb-3">
+        <?php foreach ($products as $key => $product) { ?>
+            <div class="col-3 p-2 text-center">
+                <h5 class="text-center text-danger"><?php echo $product['name'] ?></h5>
+                <a href="product.php?hidID=<?=$product['id']?>"><img src="image/<?php echo $product['img']; ?>" width="100%" height="50%"></a>
+                <p><strong><?php echo $product['price'] ?> TK</strong></p>
+                <a href="product.php?hidID=<?=$product['id']?>">
+                    <input type="submit" name="preview" value="Details" class="btn btn-outline-dark border-success mb-3 mr-2">
+                </a>
+                 <p><em><?php echo $product['quantity'];?> are available</em></p>
+                <form action="addtocart.php" method="POST">
+                    <input type="hidden" name="productId" value="<?=$product['id']?>">
+                    <input type="number" min="0" value="1" name="quantity">
+                    <input type="submit" value="add to cart" name="addToCart" class="btn btn-info border-red mt-2">
+                </form>
+            </div>
+        <?php } ?>
+        </div>
     </div>
     <?php include('layout/footer.php');?>
--- product.php
+++ product.php
@@ -1,6 +1,5 @@
 <?php
 require('src/dbconnect.php');
-
 //fetch all information
     try {
 	$stmt = $dbconnect->prepare("SELECT name,description,price,img,quantity FROM products
@@ -30,30 +29,22 @@ require('src/dbconnect.php');
                 <img src="image/IMG_1296.jpg" alt="" width="100%" height="70%" class="mt-2 mb-2">
             </div>
         </div>
-        <div class="row">
+        <div class="row text-center">
             <div class="col-4 text-center">
-               <h5 class="text-danger"><?php echo $post['name'];?></h5>
                 <img src="image/<?php echo $post['img'];?>" width="100%" height="95%" class="mt-2">
-                <a href="index.php"><input type="submit" value="Back" class="btn btn-info border-dark mb-2"></a>
             </div>
             <div class="col-4">
+               <h5 class="text-danger"><?php echo $post['name'];?></h5><hr>
                <h2 class="text-center">Specification<hr></h2>
                 <p class="text-justify">
                     <?php echo $post['description'];?>
                 </p>
             </div>
-            <div class="col-4">
-                <h4 class="text-center mt-3"><?php echo $post['price'];?> taka/item</h4><br>
-                <p class="text-center"><?php echo $post['quantity'];?> are available</p>
-                <hr>
-                
-                <form action="addtocart.php" method="POST" class="text-center">
-                    <input type="hidden" name="productId" value="<?=$product['id']?>">
-                    <input type="number" min="0" value="1" name="quantity"><br>
-                    <input type="submit" value="addtocart" name="addToCart" class="btn btn-info border-red mt-2">
-                </form>
+            <div class="col-4 text-center">
+                <h4 class="mt-3"><?php echo $post['price'];?> taka/item</h4><hr>
+                <p><?php echo $post['quantity'];?> are available</p><hr>
+                <a href="index.php"><input type="submit" value="Back" class="btn btn-info border-dark mb-2"></a>
             </div>
         </div>
-        
     </div><br><br><br>
     <?php include('layout/footer.php'); ?>
